a821c9f51943901197a95147e0ad5bb7e135e860
Jamie Beach <jamie.beach@gmail.com>
2020-07-07 19:17:52 -0400

    Created this one on Canada Day 2020

---
+++ Oh Canada.rb
@@ -0,0 +1,7 @@
+use_synth :dpulse
+play_pattern_timed [:e, :g, :g, :c], [1, 0.75, 0.25, 1.5]
+play_pattern_timed [:d, :e, :f, :g, :a, :d], [0.5, 0.5, 0.5, 0.5, 0.5, 2]
+play_pattern_timed [:e, :gb, :gb, :g], [1, 0.75, 0.25, 1.5]
+play_pattern_timed [:a, :b, :b, :a, :a, :g], [0.5, 0.5, 0.5, 0.5, 0.5, 1.5]
+play_pattern_timed [:d, :e, :f, :e, :d, :e, :f, :g, :f, :e], [0.25, 0.25, 0.75, 0.25, 0.5, 0.25, 0.25, 0.75, 0.25, 0.5]
+play_pattern_timed [:e, :d, :g, :g, :gb, :e, :gb, :g], [0.5, 0.5, 0.5, 0.25, 0.25, 0.25, 0.25, 1.5]
